7ce1fdebd1a66b79c84d5ee1c0e976f5eb550087
Mohith kumar <murugan28aug@gmail.com>
2023-05-17 03:16:35 +0530

    final

--- components/shifts/Shifts.jsx
+++ components/shifts/Shifts.jsx
@@ -14,7 +14,8 @@ const Shifts = () => {
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
-          <Image width={200} height={200} className="h-56 rounded w-full object-cover object-center mb-6" src="/assets/application.jpg" alt="content"/>
+          <Image  placeholder="blur"
+     blurDataURL="/assets/application.jpg" width={200} height={200} className="h-56 rounded w-full object-cover object-center mb-6" src="/assets/application.jpg" alt="content"/>
           {/* <img className="h-56 rounded w-full object-cover object-center mb-6" src="/assets/application.jpg" alt="content"/> */}
           {/* <h3 className="tracking-widest text-green-500 text-xs font-medium title-font">SUBTITLE</h3> */}
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Simple Process</h2>
@@ -23,7 +24,8 @@ const Shifts = () => {
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
-          <Image width={200} height={200} className="h-56 rounded w-full object-cover object-center mb-6" src="/assets/alerts.jpg" alt="content"/>
+          <Image  placeholder="blur"
+     blurDataURL="/assets/alerts.jpg" width={200} height={200} className="h-56 rounded w-full object-cover object-center mb-6" src="/assets/alerts.jpg" alt="content"/>
           {/* <h3 className="tracking-widest text-green-500 text-xs font-medium title-font">SUBTITLE</h3> */}
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Timely Alerts</h2>
           <p className="leading-relaxed text-base">Get alerted of upcoming shifts and choose from a range of shifts from various companies after being accepted.</p>
